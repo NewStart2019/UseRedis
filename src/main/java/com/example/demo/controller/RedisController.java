dd58d1d9fc171a09be7a29090270b71f05d6c37d
zhanqh50 <zhanqh50@chinaunicom.cn>
2022-06-06 08:42:49 +0800

    '配置获取客户端真实ip、port'

--- src/main/java/com/example/demo/controller/RedisController.java
+++ src/main/java/com/example/demo/controller/RedisController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
